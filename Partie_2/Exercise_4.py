cfab5064fcda14aa8ced14d4186304b7945ad90e
Voxold <bilalajaha@gmail.com>
2024-02-05 22:28:11 +0100

    P.N

--- Partie_2/Exercise_4.py
+++ Partie_2/Exercise_4.py
@@ -1,6 +1,10 @@
+# POSITIF et NEGATIF
+ 
 A = int(input("Veuillez le premier numero :"))
 
 if A > 0 :
     print("Le numero est POSITIF")
-else :
+elif A < 0 :
     print("Le numero est NEGATIF")
+else:
+    print("Le numero est NULL")
