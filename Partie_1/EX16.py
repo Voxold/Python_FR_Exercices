6951d72d41b6972e6af54368fdb3ec053927d70c
Voxold <bilalajaha@gmail.com>
2024-02-05 22:18:02 +0100

    notes

--- Partie_1/EX16.py
+++ Partie_1/EX16.py
@@ -1,5 +1,4 @@
-
-# Dans cette vidéo, nous allons écrire un programme permettant de saisir trois notes
+# Nous allons écrire un programme permettant de saisir trois notes
 # (sur 20) d'un étudiant, calculant sa moyenne et affichant cette moyenne avec la mention 
 # ("Très bien" à partir de 16, "Bien" entre 14 et 16, "Assez bien" entre 12 et 14, 
 # "Passable" entre 10 et 12, "Insuffisant" en dessous de 10)
