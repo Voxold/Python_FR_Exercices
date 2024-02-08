ed516cb820015b8ac0d41654e28207c27144e1ee
Voxold <bilalajaha@gmail.com>
2024-02-08 13:38:48 +0100

    S=1+2+3+...+10

--- Partie_3/Exercise_18.py
+++ Partie_3/Exercise_18.py
@@ -1,5 +1,5 @@
 # Écrire un programme Python qui permet de  calculer  la somme  
-#S=1+2+3+...+ 10. Utilisant la boucle for.   
+# S=1+2+3+...+10. Utilisant la boucle for.   
 
 S = 0
 for i in range(1,11) :
