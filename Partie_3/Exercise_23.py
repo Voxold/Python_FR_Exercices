80dce3e0b1f964ac6b76ec37b97a412d87de5a67
Voxold <bilalajaha@gmail.com>
2024-02-08 13:41:21 +0100

    N.parfait

--- Partie_3/Exercise_23.py
+++ Partie_3/Exercise_23.py
@@ -1,11 +1,11 @@
-#  Écrivez un programme Python pour entrer un nombre et vérifiez si 
-#le nombre est parfait ou non.
-#Un nombre parfait est un entier positif qui est égal à la somme de ses
-#diviseurs positifs appropriés.
-#Par exemple: 6 est le premier nombre parfait
-#Les diviseurs appropriés de 6 sont 1, 2, 3.
-#Somme de ses diviseurs stricts = 1 + 2 + 3 = 6.
-#Par conséquent, 6 est un nombre parfait.   
+# Écrivez un programme Python pour entrer un nombre et vérifiez si 
+# le nombre est parfait ou non.
+# Un nombre parfait est un entier positif qui est égal à la somme de ses
+# diviseurs positifs appropriés.
+# Par exemple: 6 est le premier nombre parfait
+# Les diviseurs appropriés de 6 sont 1, 2, 3.
+# Somme de ses diviseurs stricts = 1 + 2 + 3 = 6.
+# Par conséquent, 6 est un nombre parfait.   
 
 N = int(input("Saisir un nombre : "))
 
