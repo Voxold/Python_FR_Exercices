15cb7f4345d22eaac88f51054e22f5ba2f3a7033
Voxold <bilalajaha@gmail.com>
2024-02-08 13:37:46 +0100

    bonjour x10

--- Partie_3/Exercise_17.py
+++ Partie_3/Exercise_17.py
@@ -1,4 +1,4 @@
-# Écrire un programme Python qui permet d'afficher  le message
+# Écrire un programme Python qui permet d'afficher le message
 # "bonjour" 10 fois . Utilisant la  boucle for.   
 
 for i in range(1,11) :
